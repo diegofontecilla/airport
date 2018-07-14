ad18266b9c191d6099fb6896f15909c3daa8114c
diego fontecilla <mail@diegofontecilla.com>
2018-07-14 16:06:03 +0100

    Airport can instruct a plane to take off and confirm is not longer at airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,4 @@
 class Airport
-
   attr_reader :planes
 
   def initialize
@@ -8,5 +7,16 @@ class Airport
 
   def land(plane)
     @planes << plane
+  end
+
+  def take_off(plane)
+    @planes.delete(plane)
+    confirm_take_off
+  end
+
+private
+
+  def confirm_take_off
+    "British 304 is not longer at airport"
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,6 +6,23 @@ describe Airport do
     airport = Airport.new
     plane = Plane.new
     airport.land(plane)
+    # fix when airport can land many planes
     expect(airport.planes).not_to be_empty
+  end
+
+  it 'can instruct a plane to take off' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    airport.take_off(plane)
+    # fix when airport can land many planes
+    expect(airport.planes).to be_empty
+  end
+
+  it 'confirm that a plane is not longer at airport after take_off' do
+    airport = Airport.new
+    plane = Plane.new
+    airport.land(plane)
+    expect(airport.take_off(plane)).to eq("British 304 is not longer at airport")
   end
 end
